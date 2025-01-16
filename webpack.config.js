792716a617e356f07ca825a3164e3a8236b86d80
worriedape <144045807+worriedape@users.noreply.github.com>
2025-01-16 10:32:34 -0600

    finishing setup

--- webpack.config.js
+++ webpack.config.js
@@ -9,4 +9,9 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
 };
